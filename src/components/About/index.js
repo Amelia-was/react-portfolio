d5be9ffe87282aeb5131733435461ef8f249a704
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-22 09:11:21 -0400

    about component

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <h1>About</h1>
+            <p>I am so cool and nice</p>
+        </section>
+    );
+};
+
+export default About;
