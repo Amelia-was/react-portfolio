da777e5d7e7a52d63a9a0515bd80dfeb87ed81e6
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-22 09:58:30 -0400

    nav links render correct section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
+import Work from './components/Work';
+import Contact from './components/Contact';
 
 function App() {
   const navLinks = ['About', 'Work', 'Contact'];
   const [ currentPage, setCurrentPage ] = useState(navLinks[0]);
+
+  const renderPage = () => {
+    switch (currentPage) {
+        case 'Work':
+          return <Work />;
+        case 'Contact':
+          return <Contact />;
+        default:
+            return <About />;
+    }
+  }
+
   return (
     <main>
       <Nav 
       navLinks={navLinks} 
       currentPage={currentPage} 
       setCurrentPage={setCurrentPage}
-      ></Nav>
+      />
+      {renderPage()}
     </main>
   );
 };
